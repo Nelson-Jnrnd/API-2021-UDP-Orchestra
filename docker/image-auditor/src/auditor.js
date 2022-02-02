389a5ead958694bfa8ebd1c06d896eec24fe5392
nelsonJnrnd <nelsonjeanrenaud@gmail.com>
2022-02-02 10:42:32 +0100

    auditors now keep track of musician heard

--- docker/image-auditor/src/auditor.js
+++ docker/image-auditor/src/auditor.js
@@ -1,4 +1,5 @@
 var udp = require('dgram');
+var Instruments = require('./instruments');
 
 class Auditor {
     musicians_heard = {};
@@ -18,13 +19,29 @@ class Auditor {
         this.udpServer.on('message', (msg) => {
             const message = JSON.parse(msg)
             console.log(message);
+            this.addMusician(message.id, message.sound);
         })
     }
 
-    addMusician(id){
-        if(this.musicians_heard){
+    addMusician(id, sound){
+        var timeOfAddition = Date.now();
+        var instrumentHeard = Object.keys(Instruments).find(key => Instruments[key].sound == sound);
+        
+        if(!instrumentHeard)
+            instrumentHeard = 'unknown';
 
+        if(this.musicians_heard[id]){
+            this.musicians_heard[id].activeLast = timeOfAddition;
+            this.instrument = instrumentHeard;
+        } else{
+            this.musicians_heard[id] = {
+                "uuid" : id,
+                "instrument" : instrumentHeard,
+                "activeSince" : timeOfAddition,
+                "activeLast" : timeOfAddition
+            }
         }
+        console.log(this.musicians_heard);
     }
 
 }
--- docker/image-musician/src/musician.js
+++ docker/image-musician/src/musician.js
@@ -1,14 +1,17 @@
 var udp = require('dgram');
+const { v4: uuidv4} = require('uuid');
 
 class Musician {
 
     instrument;
     playing;
     musician_socket;
+    id;
     constructor(instrument) {
         this.musician_socket = udp.createSocket('udp4');
         this.instrument = instrument;
         this.startPlaying();
+        this.id = uuidv4();
     }
     startPlaying() {
         this.playing = setInterval(() => {
@@ -20,7 +23,9 @@ class Musician {
     }
     play() {
         console.log(this.instrument.sound);
-        var data = JSON.stringify(this.instrument.sound);
+        var data = JSON.stringify({
+            id : this.id,
+            sound : this.instrument.sound});
         this.musician_socket.send(data, 0, data.length, 2205, '230.185.192.108', function(err, byte) {
             console.log("sending music");
         });
